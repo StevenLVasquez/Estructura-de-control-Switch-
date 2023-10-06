7578d2eb9a0fa63bca6bb72b860bebd24f662fb5
Steven Vasquez <stevenvasquez652@gmail.com>
2023-10-06 12:22:17 -0400

    switch anidado agregado steven

--- Estructura de control ( Switch )/Program.cs
+++ Estructura de control ( Switch )/Program.cs
@@ -2,6 +2,7 @@
 //Console.WriteLine("Hello, World!");
 
 using System;
+using static System.Math;
 namespace estructuraswitch
 
 {
@@ -11,37 +12,102 @@ namespace estructuraswitch
 
        {
 
+ 
 
-
-        Console.WriteLine("Ingrese un valor entre 1 y 4");
+        Console.WriteLine("Ingrese un valor entre 1 y 5");
         int estructura = int.Parse(Console.ReadLine());
 
+            do
+                switch (estructura)
 
-        switch (estructura)
-
-        {
+                {
 
 
-            case 1:
-                Console.WriteLine("Hola mundo");
-                break;
-            case 2:
-                Console.WriteLine("Bienvenido al sistema");
-                break;
-            case 3:
-                Console.WriteLine("somos libres");
-                break;
-            case 4:
-                Console.WriteLine("logramos arregarlo");
-                break;
+                    case 1:
+                        Console.WriteLine("Hola mundo");
 
-            default:
-                Console.WriteLine("esta opción no existe");
-                    
-                break;
+                        switch (estructura)
+                        {
+                            case 1:
+                                Console.WriteLine("Hola mundo");
+                                Console.WriteLine("Seleccione una operación:");
+                                Console.WriteLine("1 - Suma");
+                                Console.WriteLine("2 - Resta");
+                                Console.WriteLine("3 - Multiplicación");
+                                Console.WriteLine("4 - División");
+                                int operacion = int.Parse(Console.ReadLine());
+
+                                switch (operacion)
+                                {
+                                    case 1:
+                                        Console.WriteLine("Ingrese dos números para sumar:");
+                                        double num1 = double.Parse(Console.ReadLine());
+                                        double num2 = double.Parse(Console.ReadLine());
+                                        double suma = num1 + num2;
+                                        Console.WriteLine($"Resultado: {suma}");
+                                        break;
+                                    case 2:
+                                        Console.WriteLine("Ingrese dos números para restar:");
+                                        num1 = double.Parse(Console.ReadLine());
+                                        num2 = double.Parse(Console.ReadLine());
+                                        double resta = num1 - num2;
+                                        Console.WriteLine($"Resultado: {resta}");
+                                        break;
+                                    case 3:
+                                        Console.WriteLine("Ingrese dos números para multiplicar:");
+                                        num1 = double.Parse(Console.ReadLine());
+                                        num2 = double.Parse(Console.ReadLine());
+                                        double multiplicacion = num1 * num2;
+                                        Console.WriteLine($"Resultado: {multiplicacion}");
+                                        break;
+                                    case 4:
+                                        Console.WriteLine("Ingrese dos números para dividir:");
+                                        num1 = double.Parse(Console.ReadLine());
+                                        num2 = double.Parse(Console.ReadLine());
+
+                                        if (num2 != 0)
+                                        {
+                                            double division = num1 / num2;
+                                            Console.WriteLine($"Resultado: {division}");
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("No se puede dividir por cero.");
+                                        }
+                                        break;
+                                    default:
+                                        Console.WriteLine("Operación no válida.");
+                                        break;
+                                }
+                                break;
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Bienvenido al sistema");
+                        break;
+                    case 3:
+                        Console.WriteLine("somos libres");
+                        break;
+                    case 4:
+                        Console.WriteLine("logramos arregarlo");
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Cerrar programa");
+                        break;
+
+
+
+                    default:
+                        Console.WriteLine("esta opción no existe");
+
+                        break;
+                }
+
+            while (estructura != 5);
+            
+
         }
-
-       }
         //hola soy steven , steven hizp este codigo 
 
     }
